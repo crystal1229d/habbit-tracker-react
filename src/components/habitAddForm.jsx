9b0ef87b7eb8d5f3c08c385225b689296f3c5afc
crystal1229d <crystal1229d@naver.com>
2021-04-03 02:38:50 +0900

    2021-04-02(FRI)

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 
-class HabitAddForm extends Component {
-    inputRef = React.createRef();   // DOM요소에 바로 접근X. 리액트에서 제공하는 Ref이용 
-    formRef = React.createRef();
+const HabitAddForm = memo((props) => {
+    // 함수에서는 this를 쓰지 않아도 바로 접근 가능 => 코드가 깔끔해지기 때문에 함수를 선호하는 경향이 있다.
+    const inputRef = React.createRef();   // DOM요소에 바로 접근X. 리액트에서 제공하는 Ref이용 
+    const formRef = React.createRef();
 
-    onSubmit = event => {
+    const onSubmit = event => {
         event.preventDefault(); // onSubmit 발생시 브라우저 기본 동작(리프레쉬) 방지
-        const name = this.inputRef.current.value;
-        name && this.props.onAdd(name);
+        const name = inputRef.current.value;
+        name && props.onAdd(name);
         // this.inputRef.current.value = '';
-        this.formRef.current.reset();
+        formRef.current.reset();
     };
 
-    render() {
-        return (
-            <form ref={this.formRef} className="add-form" onSubmit={this.onSubmit}>
-                <input ref={this.inputRef} type="text" className="add-input" placeholder="Enter Your Habit" />
-                <button className="add-button">Add</button>
-            </form>
-        );
-    }
-}
+    return (
+        <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+            <input ref={inputRef} type="text" className="add-input" placeholder="Enter Your Habit" />
+            <button className="add-button">Add</button>
+        </form>
+    );
+});
 
 export default HabitAddForm;

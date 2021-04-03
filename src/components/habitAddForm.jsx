5d483ef3255c23944744fb089481909a05ddb036
crystal1229d <crystal1229d@naver.com>
2021-04-03 14:11:15 +0900

    2021-04-03(SAT)

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react';
 
 const HabitAddForm = memo((props) => {
+    // memo : props가 변경되지 않으면 안에 잇는 함수가 호출되지 않는다. state가 따로없다면 함수형 PureComponent 만들기
     // 함수에서는 this를 쓰지 않아도 바로 접근 가능 => 코드가 깔끔해지기 때문에 함수를 선호하는 경향이 있다.
     const inputRef = React.createRef();   // DOM요소에 바로 접근X. 리액트에서 제공하는 Ref이용 
     const formRef = React.createRef();
